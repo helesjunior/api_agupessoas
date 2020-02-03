f6c2758ac4e47736ddba8751f1c16307e538e69e
Heles Resende Silva Junior <heles.junior@AGU.GOV.BR>
2020-02-03 10:16:55 -0300

    teste de acesso

--- app/Http/Controllers/Api/ServidorController.php
+++ app/Http/Controllers/Api/ServidorController.php
@@ -15,12 +15,13 @@ class ServidorController extends Controller
 {
     public function buscaServidorPorCpf(Integer $cpf)
     {
-        dd('Entrei', $cpf);
+        dd($cpf);
 
         $servidor = Servidor::whereHas('documentacao', function ($query) use ($cpf){
             $query->where('nr_documentacao',$cpf)
             ->where('id_tipo_documentacao',1);
         })->get();
+
 
         return json_encode($servidor);
     }
